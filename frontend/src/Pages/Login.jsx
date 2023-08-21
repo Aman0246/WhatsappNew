8219e454112dd66948f967f236ec45d8adfc8003
Aman0246 <amankashyap0246@gmail.com>
2023-08-21 22:15:07 +0530

    ??

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -2,86 +2,81 @@ import styled from '@emotion/styled'
 import { Box, Button, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import {auth,provider} from "../Firebase/googleLogin"
-import {signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../Firebase/googleLogin"
+import { signInWithPopup } from "firebase/auth";
 import { motion, AnimatePresence } from "framer-motion"
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
-const LoginWrapper=styled(Box)({
-    // backgroundColor:'#f4f5f8',
-  height:'90vh'
-  ,width:'90%',
+import { loginFunction } from '../../Axios/axiosrequests'
+const LoginWrapper = styled(Box)({
+  // backgroundColor:'#f4f5f8',
+  height: '90vh'
+  , width: '90%',
   display: "flex",
-  borderRadius:'20px',
-  backgroundColor:'aliceblue',
-  justifyContent:'center',
-  alignItems:'center',
-  margin:'auto'
+  borderRadius: '20px',
+  backgroundColor: 'aliceblue',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 'auto'
 })
 
 
 export default function Login() {
-  const [inputdata,setinputdata]=useState({})
-  const navigate=useNavigate()
-  const handleChange=(e)=>{
-    setinputdata({...inputdata,[e.target.name]:e.target.value})
-  
+  const [inputdata, setinputdata] = useState({})
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setinputdata({ ...inputdata, [e.target.name]: e.target.value })
+
   }
-  const handleGoogleLogin=async()=>{
+  const handleGoogleLogin = async () => {
     signInWithPopup(auth, provider)
-    .then(async(result) => {
-      await axios.post('/googleLogin',result.user).then((e)=>{
-        if(e.data.status==true){
-          localStorage.setItem("token",e.data.token)
-          toast.success(e.data.message)
-          navigate('/welcomePage')
-        }
-        else toast('server down')
-// console.log(e)
-      })
+      .then(async (result) => {
+        await axios.post('/googleLogin', result.user).then((e) => {
+          if (e.data.status == true) {
+            localStorage.setItem("token", e.data.token)
+            toast.success(e.data.message)
+            navigate('/welcomePage')
+          }
+          else toast('server down')
+          // console.log(e)
+        })
       }).catch((error) => {
-        toast('server down',error)
-        
+        toast('server down', error)
+
       });
   }
- 
-const handleLogin=async()=>{
-  try {
-    await axios.post("/login",inputdata).then(async(e)=>{
-      // console.log(e.data.token)  
-       if(e.status===200){toast.success(e.data.message) 
-        localStorage.setItem("token",e.data.token)
-        navigate('/welcomePage')}
-       else toast.error('server Down')
-     } )
-     } catch (error) {
-       if(error.response.data
-         .message)
-      return toast.error(error.response.data
-         .message)
-      
-     }
-}
+
+  const handleLogin = async () => {
+    try {
+      let data = await loginFunction(inputdata)
+      if (data == true) {
+        navigate('/welcomePage')
+      }
+    } catch (error) {
+      console.error(error);
+     
+    }
+  };
 
 
   return (
     <LoginWrapper>
-      <motion.div initial={{opacity:0,scale:0}} animate={{opacity:1,scale:1}} exit={{opacity:0,scale:0}} transition={{ease:"anticipate",duration:".2"}} style={{backgroundColor:'white',boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',borderRadius:'20px',opacity:.8,width:'30%','@media (max-width:763px)': {width:'80%'},paddingBottom:5}}> 
-        <Box sx={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',gap:5}}>
-        <TextField onChange={handleChange} sx={{marginTop:5,width:'60%'}} required name='email'  type='email' color='success' id="standard-basic" label="Email" variant="standard" />
-        <TextField onChange={handleChange} sx={{width:'60%'}} color='success' name='password'  type='Password' id="standard-basic" label="Password" variant="standard" />
-        <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',gap:2}}>
+      <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0 }} transition={{ ease: "anticipate", duration: ".2" }} style={{ backgroundColor: 'white', boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px', borderRadius: '20px', opacity: .8, width: '30%', '@media (max-width:763px)': { width: '80%' }, paddingBottom: 5 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 5 }}>
+          <TextField onChange={handleChange} sx={{ marginTop: 5, width: '60%' }} required name='email' type='email' color='success' id="standard-basic" label="Email" variant="standard" />
+          <TextField onChange={handleChange} sx={{ width: '60%' }} color='success' name='password' type='Password' id="standard-basic" label="Password" variant="standard" />
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2 }}>
 
-        <Button onClick={handleLogin} variant="contained" sx={{backgroundColor:'teal'}}>
-        Login
-      </Button>
-      Or
-      <Button onClick={handleGoogleLogin}  sx={{backgroundColor:'white', boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',color:'#2b8687',fontWeight:600,"&":{border:"1px solid gray",borderRadius:'50px'}}}> <img style={{width:"1.5rem",height:'1.5rem',marginRight:'5px'}} src="https://w7.pngwing.com/pngs/543/934/png-transparent-google-app-logo-google-logo-g-suite-google-text-logo-circle.png" alt="GoogleButton"  />Continue With Google</Button>
-      <Box sx={{fontWeight:600}}>New User ?<Link to={'/registration'}><span style={{color:"#309899",cursor:'pointer'}}> Register</span></Link></Box>
-        </Box>
+            <Button onClick={handleLogin} variant="contained" sx={{ backgroundColor: 'teal' }}>
+              Login
+            </Button>
+            Or
+            <Button onClick={handleGoogleLogin} sx={{ backgroundColor: 'white', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px', color: '#2b8687', fontWeight: 600, "&": { border: "1px solid gray", borderRadius: '50px' } }}> <img style={{ width: "1.5rem", height: '1.5rem', marginRight: '5px' }} src="https://w7.pngwing.com/pngs/543/934/png-transparent-google-app-logo-google-logo-g-suite-google-text-logo-circle.png" alt="GoogleButton" />Continue With Google</Button>
+            <Box sx={{ fontWeight: 600 }}>New User ?<Link to={'/registration'}><span style={{ color: "#309899", cursor: 'pointer' }}> Register</span></Link></Box>
+          </Box>
         </Box>
       </motion.div>
-    
+
     </LoginWrapper>
   )
 }
